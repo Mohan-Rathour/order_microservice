558895b8238e2e5437304c818d0bef4fe591f6eb
Mohan Rathour <mohanpratap.singh@excelindia.com>
2021-11-16 17:00:51 +0530

    revert the port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8080;
 let routes = require('./routes/routes');
 
 routes(app);
